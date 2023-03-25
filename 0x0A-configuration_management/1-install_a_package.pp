1dfd20b62285a674febafdfdb3c41f0344014bd9
Kelvin Chaula <83451842+naingolan@users.noreply.github.com>
2023-03-25 05:00:17 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# this Puppet manifest installs the puppet-lint package
+package {'puppet-lint':
+ensure   => '2.1.0',
+provider => 'gem',
 }
